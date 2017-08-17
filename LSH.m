c36ddf8efe4c0eddb48e13b096b263b7f19ece35
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2017-08-17 22:39:08 +0200

    Refactoring of LSH

--- LSH.m
+++ LSH.m
@@ -2,7 +2,5 @@ function [lsh_weights] = LSH(d, b)
   lsh_weights = stdnormal_rnd(d, b);
   % Normalization of rows
   norms = sqrt(sumsq(lsh_weights, 2));
-  for r = 1:d
-      lsh_weights(r, :) = lsh_weights(r, :)./norms(r);
-   endfor 
+  lsh_weights = lsh_weights./norms;
 end
